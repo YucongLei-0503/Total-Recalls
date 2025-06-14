2df85fee97430f65cce55a40c7cbcf2a0159ab6b
Khola Jamshad <kjamshad@l-ph3w0fq4hy.local>
2025-06-13 23:16:06 -0700

    updated cleaning functions

--- cleaning.py
+++ cleaning.py
@@ -21,9 +21,8 @@ def check_duplicates(df, columns):
 
 
 
-# function to remove duplicates
+# function to remove duplicates based on the columns specified
 def remove_duplicates_and_save(df, cols):
-    # remove if all columns except the last one (RID) are duplicates
     df = df.loc[~df.duplicated(subset=cols, keep='first')]
 
     return df
@@ -34,7 +33,6 @@ def remove_duplicates_and_save(df, cols):
 
 # function to drop unnecessary columns
 def drop_unnecessary_columns(drop_list, df):
-    # drop unnecessary columns
     df.drop(columns=drop_list, inplace=True, errors='ignore')
 
     return df
@@ -137,3 +135,89 @@ def list_unique_entries(df, column):
     # list unique entries in the specified column
     unique_entries = df[column].unique()
     print(unique_entries)
+    # count of each unique entry
+    counts = df[column].value_counts()
+    print(counts)
+
+
+
+
+# function to categorize DISTRIBUTIONAREASUMMARYTXT column
+def distribution_area(df, states, states_abbr):
+    
+    # map of 50 US states to their abbreviations
+    state_map = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"}
+    # function to categorize states
+    def categorize_state(dist):
+        states_found = ""
+        for state in states:
+            if pd.notnull(dist) and state in dist:
+                # map state to its abbreviation using the state_map
+                states_found = states_found + state_map[state] + ', '
+
+        for state_abbr in states_abbr:
+            if pd.notnull(dist) and state_abbr in dist:
+                states_found = states_found + state_abbr + ', '
+        return states_found if states_found else ""
+    
+        # None: no states identified i.e only number of centers or states given, or no information
+
+            
+    # create a new column with the categorized states
+    df['DISTRIBUTION'] = df['DISTRIBUTIONAREASUMMARYTXT'].apply(categorize_state)
+
+    # Covers cases where distribution is nationwide
+    df.loc[df['DISTRIBUTION'].isnull() & df['DISTRIBUTIONAREASUMMARYTXT'].str.contains('nationwide|all 50|national', case=False, na=False), 'DISTRIBUTION'] = 'US'
+
+    return df
+
